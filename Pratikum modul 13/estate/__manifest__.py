67a862f4e7f3131decc62eb4508f5a35902dcbfe
Robert Tijani Saifunnawas <90190959+roberttijani@users.noreply.github.com>
2023-06-14 14:41:18 +0700

    pratikum 13

---
+++ Pratikum modul 13/estate/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Estate 01",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Proweb Indinesia",
+    'application': True,
+    'data':[
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+    ],
+}
